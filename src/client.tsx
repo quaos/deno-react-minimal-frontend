e4f24fc6526495aed657f2d57b680d3a462fe339
Chakrit W <quaos.qrz@gmail.com>
2022-01-04 12:55:39 +0700

    Test Deno NS issue with std version 0.119.0

--- src/client.tsx
+++ src/client.tsx
@@ -1,4 +1,10 @@
 import { React, ReactDOM } from "./deps/react.ts";
+//TEST {
+import { encode } from "./deps/std.ts";
+console.log("encode:", encode);
+// import { getSystemErrorName } from "./deps/std.ts";
+// console.log("getSystemErrorName:", getSystemErrorName);
+// }
 
 import App from "./components/App.tsx";
 
--- src/deps/std.ts
+++ src/deps/std.ts
@@ -1,1 +1,5 @@
-export { assert, assertEquals } from "https://deno.land/std@0.99.0/testing/asserts.ts";
+export { assert, assertEquals } from "https://deno.land/std@0.119.0/testing/asserts.ts";
+
+//TEST
+export { encode } from "https://deno.land/std@0.119.0/node/querystring.ts";
+// export { getSystemErrorName } from "https://deno.land/std@0.119.0/node/util.ts";
